d6bf7cbc1030da6c07f513f02e27013d43a26965
Massamba Sow <mass.sow@gmail.com>
2021-03-21 22:16:59 -0500

    feat: add install option for pyenv-install-latest plugin

--- shared/clone-repos.sh
+++ shared/clone-repos.sh
@@ -17,7 +17,10 @@ function clone () {
   # Blog
 
   # Products
-  
+
+  # Plugins
+    # pyenv-install-latest
+    git clone https://github.com/momo-lab/pyenv-install-latest.git "$(pyenv root)"/plugins/pyenv-install-latest 
 }
 
 clone
