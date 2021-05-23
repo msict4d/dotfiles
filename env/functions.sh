083e58805a8f1408fedaa081f9483d09940c527c
Massamba Sow <mass.sow@gmail.com>
2021-05-23 18:41:27 -0500

    feature: promote PATH functions to be loaded by default

--- env/functions.sh
+++ env/functions.sh
@@ -552,3 +552,27 @@ if [ $? -eq 0 ]; then
 		git diff --no-index --color-words "$@";
 	}
 fi;
+
+# function to remove duplicates in and echo $PATH
+### no_dupes_path()
+# Note: Inspired from https://www.linuxjournal.com/content/removing-duplicate-path-entries
+no_dupes_path() {
+  local no_dupes_path
+  no_dupes_path=$(echo $PATH | awk -v RS=: '!($0 in a) {a[$0]; printf("%s%s", length(a) > 1 ? ":" : "", $0)}')
+  echo "$no_dupes_path"
+}
+
+# function to echo $PATH one entry per line
+### no_dupes_path()
+# Note: Inspired from https://www.linuxjournal.com/content/removing-duplicate-path-entries
+show_path(){
+  awk -v RS=: '{print}' <<< "$PATH"
+}
+
+# function to set $PATH with no dupes and echo one entry per line
+### no_dupes_path()
+# Note: Inspired from https://www.linuxjournal.com/content/removing-duplicate-path-entries
+set_no_dupes_path() {
+  export PATH=$(no_dupes_path)
+  show_path
+}
